ba35222bc741796f72b730350cf0471386d3a17e
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-11-08 18:55:59 +0600

    jwt using successfully

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -7,8 +9,12 @@ const port = process.env.PORT || 5000;
 
 //middleware
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 console.log(process.env.DB_USER);
 
@@ -24,16 +30,63 @@ const client = new MongoClient(uri, {
     }
 });
 
+// own create middleware
+
+const logger = async (req, res, next) => {
+    console.log('called', req.hostname, req.originalUrl)
+    next();
+}
+
+//verify token 
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('value of token in middleware', token)
+    if (!token) {
+        return res.send({ message: 'not authorized' })
+        
+    }
+    next();
+    // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded) =>{
+    //     //error
+    //     if (err) {
+    //         console.log(err);
+    //         return res.status(401).send({ message: 'unauthorized' })
+    //     }
+    //     console.log('value in the token', decoded)
+
+    //     req.user = decoded;
+    //     next();
+    // })
+
+    
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-       
+
         const userCollection = client.db('userfoodshop').collection('user')
         const foodCollection = client.db('foodShop').collection('food')
 
         const foodrequstCollection = client.db('foodShop').collection('foodrequest')
+
+        //auth related api
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                    // sameSite: 'none'
+                })
+                .send({ success: true })
+
+        })
 
         //foodrequest related api
         app.post('/requestfood', async (req, res) => {
@@ -43,7 +96,7 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/requestfood', async (req, res) => {
+        app.get('/requestfood', logger, async (req, res) => {
             const cursor = foodrequstCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -55,6 +108,24 @@ async function run() {
             const result = await foodrequstCollection.findOne(query);
             res.send(result);
         });
+
+
+        //some
+
+        app.get('/foodrequest', async (req, res) => {
+            console.log(req.query.email);
+            // console.log('tok tok', req.cookies.token)
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+
+            }
+
+            const result = await foodrequstCollection.find(query).toArray();
+            console.log(result)
+            res.send(result);
+        });
+
 
         //status changed confirm
         app.patch('/requfood/:id', async (req, res) => {
@@ -84,18 +155,18 @@ async function run() {
 
 
 
-     
+
 
 
         //add food related api
         app.post('/allfood', async (req, res) => {
             const newFood = req.body;
             console.log(newFood);
-            const result= await foodCollection.insertOne(newFood);
+            const result = await foodCollection.insertOne(newFood);
             res.send(result);
         })
 
-        app.get('/allfood', async (req, res) => {
+        app.get('/allfood', logger, async (req, res) => {
             const cursor = foodCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -108,35 +179,37 @@ async function run() {
         //     res.send(result);
         // })
 
-        app.get('/onefood/:id', async (req,res) =>{
-            const id= req.params.id;
-            const query = {_id: new ObjectId(id)}
+        app.get('/onefood/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
             const result = await foodCollection.findOne(query);
             res.send(result);
         });
         //update add food
-        app.put('/onefood/:id', async(req, res) =>{
+        app.put('/onefood/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id : new ObjectId(id)}
+            console.log(id)
+            const filter = { _id: new ObjectId(id) }
             const updatedFood = req.body;
+            console.log(updatedFood)
             const uFood = {
-                $set:{
-                 
-                    foodname: updatedFood.name, 
-                    foodimage: updatedFood.foodimage, 
-                    foodquantity: updatedFood.foodquantity, 
-                    location: updatedFood.location, 
-                    date: updatedFood.date, 
-                    additionalnotes: updatedFood.additionalnotes, 
-                    foodstatus: updatedFood.foodstatus, 
-                    email:updatedFood.email,
-                    donatorimage: updatedFood.donatorimage,
-                    donatorname: updatedFood.donatorname
-                   
+                $set: {
+
+                    foodname: updatedFood.foodname,
+                    foodimage: updatedFood.foodimage,
+                    foodquantity: updatedFood.foodquantity,
+                    location: updatedFood.location,
+                    date: updatedFood.date,
+                    additionalnotes: updatedFood.additionalnotes,
+                    foodstatus: updatedFood.foodstatus,
+                    // email:updatedFood.email,
+                    // donatorimage: updatedFood.donatorimage,
+                    // donatorname: updatedFood.donatorname
+
                 }
             }
 
-            const result = await foodCollection.updateOne(filter,uFood)
+            const result = await foodCollection.updateOne(filter, uFood)
             res.send(result);
         })
 
@@ -149,40 +222,41 @@ async function run() {
         });
 
 
-        
 
-     
 
-        app.get('/somefood', async (req, res) => {
-            console.log(req.query.email);
-            let query ={};
-            if (req. query?.email){
+
+
+        app.get('/somefood', logger,verifyToken,  async (req, res) => {
+            // console.log(req.query.email);
+            // console.log('tok tok',req.cookies.token)
+            let query = {};
+            if (req.query?.email) {
                 query = { email: req.query.email }
 
             }
-            
+
             const result = await foodCollection.find(query).toArray();
             console.log(result)
             res.send(result);
         });
 
-    
-        
 
-        
 
-       
 
- 
+
+
+
+
+
 
         //user related apis
-        app.get('/user' ,async (req,res) =>{
+        app.get('/user', async (req, res) => {
             const cursor = await userCollection.find();
             const users = await cursor.toArray();
             res.send(users);
         });
 
-        app.post('/user', async(req,res) =>{
+        app.post('/user', async (req, res) => {
             const user = req.body;
             console.log(user);
             const result = await userCollection.insertOne(user);
@@ -190,7 +264,7 @@ async function run() {
         })
 
 
-      
+
 
 
 
